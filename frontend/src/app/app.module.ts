286b11282ac653467cedc3d591470266cd63222a
blancior <pkedziora347@gmail.com>
2024-04-18 00:40:14 +0200

    Feat: Data formatted & record styling

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {BrowserFormComponent} from "./components/browser-form/browser-form.component";
 import {HttpClientModule} from "@angular/common/http";
 import { FlightRecordComponent } from './components/flight-record/flight-record.component';
-
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -14,10 +17,16 @@ import { FlightRecordComponent } from './components/flight-record/flight-record.
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     BrowserFormComponent,
-    HttpClientModule
+    HttpClientModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/components/browser-form/browser-form.component.ts
+++ frontend/src/app/components/browser-form/browser-form.component.ts
@@ -1,13 +1,40 @@
 import {Component, EventEmitter, Output} from '@angular/core';
-import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {FlightService} from "../../services/flight/flight.service";
-// import {FlightService} from "../../services/flight.service";
-
+import {MatFormField, MatFormFieldModule, MatHint, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {provideMomentDateAdapter} from "@angular/material-moment-adapter";
+import * as moment from 'moment';
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'yyyy-MM-dd',
+  },
+  display: {
+    dateInput: 'yyyy-MM-DD',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 @Component({
   selector: 'app-browser-form',
   standalone: true,
   imports: [
     ReactiveFormsModule,
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatDatepicker,
+    MatFormFieldModule,
+    MatDatepickerInput,
+    MatNativeDateModule,
+    MatHint,
+    MatDatepickerToggle
+  ],
+  providers:[
+    provideMomentDateAdapter(MY_FORMATS)
   ],
   templateUrl: './browser-form.component.html',
   styleUrls: ['./browser-form.component.scss']
@@ -17,21 +44,24 @@ export class BrowserFormComponent {
   @Output() locationsSender: EventEmitter<string[]> = new EventEmitter();
   availableFlights!:Array<any>;
   locations:string[] = [];
-  flightForm = this.fb.group({
-    origin: ['', Validators.required],
-    destination: ['', Validators.required],
-    adults: [0, Validators.required],
-    dateOfDeparture: ['', Validators.required],
-    dateOfReturn: ['', Validators.required],
-  });
+  flightForm!: FormGroup;
 
   constructor(private fb: FormBuilder,
               private fs: FlightService) {
+    this.flightForm = this.fb.group({
+      origin: ['', Validators.required],
+      destination: ['', Validators.required],
+      adults: [0, Validators.required],
+      dateOfDeparture: ['', Validators.required],
+      dateOfReturn: ['', Validators.required],
+    });
   }
 
   onSubmit(){
     const originValue = this.flightForm.get('origin')!.value;
     const destination = this.flightForm.get('destination')!.value;
+    this.setFormattedData('dateOfDeparture');
+    this.setFormattedData('dateOfReturn');
     console.log(this.flightForm.value)
    if (originValue && destination){
      this.locations[0] = originValue;
@@ -53,11 +83,12 @@ export class BrowserFormComponent {
          })
        });
      });
-
-
-
-
    }
+  }
 
+  private setFormattedData(selector:string) {
+    const date: moment.Moment = moment(this.flightForm.get(selector)!.value);
+    const formattedDate: string = date.format('YYYY-MM-DD');
+    return this.flightForm.get(selector)?.setValue(formattedDate);
   }
 }
